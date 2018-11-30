291f4c35576021e9ec3f4868196c57d94fdb03e1
Mateusz Ż <thedead4fun@live.com>
2018-11-30 23:13:02 +0000

    Change icon in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
+import Icon from './Icon.jsx';
 
 const Header = (props) => (
-  <h1 {...props}>⏲ Countdown</h1>
+  <h1 {...props}><StyledIcon icon="clock-outline" /> Countdown</h1>
 );
 
 const StyledHeader = styled(Header)`
   font-size: 1.75rem;
   font-weight: 700;
   margin: 48px 0;
+`;
+
+const StyledIcon = styled(Icon)`
+  fill: #5e81ac;
+
+  & > svg {
+    transform: translateY(2px);
+  }
 `;
 
 export default StyledHeader;
--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { icons } from 'eva-icons';
 
 const Icon = (props) => (
-  <span dangerouslySetInnerHTML={{__html: icons[props.icon].toSvg()}}></span>
+  <span {...props} dangerouslySetInnerHTML={{__html: icons[props.icon].toSvg()}}></span>
 );
 
 export default Icon;
