eebc42c5ddf8c32e6a24a5403b84ef293658e786
Mateusz Ż <zochowski.mateusz@outlook.com>
2020-06-10 22:38:15 +0200

    Use html5 inputs for date and time picking

--- src/components/AddTimerForm.jsx
+++ src/components/AddTimerForm.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
-import Input from './Input.jsx';
 import Button from './Button.jsx';
 import { timersState, isFormVisibleState } from '../atoms.js';
 
 export default (props) => {
   const [name, setName] = useState('');
-  const [date, setDate] = useState('');
+  const [date, setDate] = useState(new Date());
+  const [time, setTime] = useState('');
   const [error, setError] = useState({ input: '', message: '' });
 
   const addTimer = useSetRecoilState(timersState);
@@ -23,23 +23,27 @@ export default (props) => {
     setDate(event.target.value);
   };
 
+  const handleChangeTime = (event) => {
+    if (error.input === 'time') setError({});
+    setTime(event.target.value);
+  };
+
   const createNewTimer = (event) => {
     event.preventDefault();
+    const dateTimeString = `${date} ${time}`;
 
     try {
-      const regex = /^(\d{1,4}(\s|-)\d{1,2}(\s|-)\d{1,2})|(\d{1,4}(\s|-)\d{1,2}(\s|-)\d{1,2} ([0-1][0-9]:[0-5][0-9]|2[0-3]:[0-5][0-9]))$/;
-
       if (name === '') throw 'EMPTY_NAME';
       if (date === '') throw 'EMPTY_DATE';
-      if (isNaN(Date.parse(date)) ||
-          date.match(regex) === null) throw 'INVALID_DATE';
+      if (time === '') throw 'EMPTY_TIME';
+      if (isNaN(Date.parse(dateTimeString))) throw 'INVALID_DATE';
 
       addTimer(oldTimers => [
         ...oldTimers,
         {
           id: Date.now(),
           name,
-          date: Date.parse(date),
+          date: Date.parse(dateTimeString),
         }
       ]);
 
@@ -51,6 +55,9 @@ export default (props) => {
           break;
         case 'EMPTY_DATE':
           setError({ input: 'date', message: 'EMPTY_DATE' });
+          break;
+        case 'EMPTY_TIME':
+          setError({ input: 'time', message: 'EMPTY_TIME' });
           break;
         case 'INVALID_DATE':
           setError({ input: 'date', message: 'INVALID_DATE' });
@@ -73,34 +80,52 @@ export default (props) => {
         name="name"
         placeholder="Enter name"
         onChange={handleChangeName}
-        invalid={error.input === 'name' ? 'true' : undefined}
       />
       <Container>
-        <Name>Date:</Name>
+        <Name>Date and time:</Name>
         {error.input === 'date' && <Error>Enter correct date</Error>}
+        {error.input === 'time' && <Error>Enter correct time</Error>}
       </Container>
       <Input
-        type="text"
+        type="date"
         name="date"
         placeholder="Enter date"
         onChange={handleChangeDate}
-        invalid={error.input === 'date' ? 'true' : undefined}
       />
-      <Hint>Use following date format: yyyy-mm-dd hh:mm<br />Time is optional.</Hint>
+      <Input
+        type="time"
+        name="time"
+        placeholder="Enter time (optional)"
+        onChange={handleChangeTime}
+      />
       <Button onClick={createNewTimer}>Create</Button>
     </Form>
   );
 }
 
+const Input = styled.input`
+  width: 300px;
+  box-sizing: border-box;
+  font-size: 1.2rem;
+  padding: 0.3rem 0.5rem;
+  margin: 6px 0;
+  color: #6e778a;
+  border: 2px solid #d8dee9;
+  border-radius: 3px;
+  outline: 0;
+  transition: .1s ease-in-out;
+  ::placeholder {
+    color: #d8dee9;
+  }
+  :active, :focus {
+    border: 2px solid #5e81ac;
+  }
+`;
+
 const Form = styled.form`
   width: 300px;
   margin: 0 auto;
   text-align: center;
-`;
-
-const Hint = styled.p`
-  font-size: 0.775em;
-  color: #6B7487;
 `;
 
 const Container = styled.div`
@@ -121,5 +146,5 @@ const Name = styled.span`
 
 const Error = styled.span`
   float: right;
-  color: red;
+  color: #bf616a;
 `;
